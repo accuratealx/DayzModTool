0b47a5c287807b8b2f5dfc522f950e6e904246b8
accuratealx <accuratealx@gmail.com>
2023-12-23 16:57:01 +0300

    Добавлен фрейм редактора параметра DirectoryList

---
+++ App/ParamFrames/ParamFrameDirectoryList/ParamFrameDirectoryListModEditorUnit.pas
@@ -0,0 +1,173 @@
+unit ParamFrameDirectoryListModEditorUnit;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, ExtCtrls, Buttons, StdCtrls,
+  StartParamDirectoryList;
+
+type
+  TParamFrameDirectoryListModEditorFrame = class(TFrame)
+    btnSelectAll: TSpeedButton;
+    btnUnselectAll: TSpeedButton;
+    procedure btnSelectAllClick(Sender: TObject);
+    procedure btnUnselectAllClick(Sender: TObject);
+  private
+    FItems: TStartParamDirectoryListItemArray;
+    FCheckBoxList: array of TCheckBox;
+
+    procedure SetCheckStatus(AStatus: Boolean);
+    procedure cbClick(Sender: TObject);
+
+    procedure ClearCheckBoxList;
+    procedure CreateCheckBoxList(AItems: TStartParamDirectoryListItemArray);
+    procedure CorrectFrameHeight;
+  public
+    constructor Create(AItems: TStartParamDirectoryListItemArray); reintroduce;
+    destructor  Destroy; override;
+
+    procedure UpdateInterface;
+
+    property Items: TStartParamDirectoryListItemArray read FItems;
+  end;
+
+
+implementation
+
+{$R *.lfm}
+
+
+procedure TParamFrameDirectoryListModEditorFrame.btnSelectAllClick(Sender: TObject);
+begin
+  SetCheckStatus(True);
+end;
+
+
+procedure TParamFrameDirectoryListModEditorFrame.btnUnselectAllClick(Sender: TObject);
+begin
+  SetCheckStatus(False);
+end;
+
+
+procedure TParamFrameDirectoryListModEditorFrame.SetCheckStatus(AStatus: Boolean);
+var
+  i, c: Integer;
+begin
+  c := Length(FCheckBoxList) - 1;
+  for i := 0 to c do
+    FCheckBoxList[i].Checked := AStatus;
+end;
+
+
+procedure TParamFrameDirectoryListModEditorFrame.cbClick(Sender: TObject);
+var
+  Cb: TCheckBox absolute Sender;
+begin
+  FItems.Item[Cb.Tag].Checked := Cb.Checked;
+end;
+
+
+procedure TParamFrameDirectoryListModEditorFrame.ClearCheckBoxList;
+var
+  i, c: Integer;
+begin
+  c := Length(FCheckBoxList) - 1;
+  for i := 0 to c do
+    FCheckBoxList[i].Free;
+  SetLength(FCheckBoxList, 0);
+end;
+
+
+procedure TParamFrameDirectoryListModEditorFrame.CreateCheckBoxList(AItems: TStartParamDirectoryListItemArray);
+
+  procedure Add(cb: TCheckBox);
+  var
+    c: Integer;
+  begin
+    c := Length(FCheckBoxList);
+    SetLength(FCheckBoxList, c + 1);
+    FCheckBoxList[c] := cb;
+  end;
+
+const
+  X_OFFSET = 5;
+  Y_OFFSET = 5;
+var
+  i, c, Y: Integer;
+  cb: TCheckBox;
+begin
+  ClearCheckBoxList;
+
+  c := AItems.Count - 1;
+  Y := Y_OFFSET;
+
+  for i := 0 to c do
+  begin
+    cb := TCheckBox.Create(nil);
+    cb.Cursor := crHandPoint;
+    cb.Caption := Trim(AItems.Item[i].Value);
+    cb.Checked := AItems.Item[i].Checked;
+    cb.OnClick := @cbClick;
+    cb.Tag := i;
+    cb.Left := btnSelectAll.Left + btnSelectAll.Width + X_OFFSET;
+    cb.Top := Y;
+    cb.Parent := Self;
+
+    Add(cb);
+
+    Inc(Y, cb.Height);
+  end;
+end;
+
+
+procedure TParamFrameDirectoryListModEditorFrame.CorrectFrameHeight;
+var
+  c, ItemHeight, MinHeight, CbHeight: Integer;
+begin
+  MinHeight := btnUnselectAll.Top + btnUnselectAll.Height;
+
+  c := Length(FCheckBoxList);
+
+  if c > 0 then
+    ItemHeight := FCheckBoxList[0].Height
+  else
+    ItemHeight := 0;
+
+  CbHeight := c * ItemHeight + 5;
+
+  if CbHeight > MinHeight then
+    Height := CbHeight
+  else
+    Height := MinHeight;
+end;
+
+
+constructor TParamFrameDirectoryListModEditorFrame.Create(AItems: TStartParamDirectoryListItemArray);
+begin
+  inherited Create(nil);
+
+  FItems := AItems;
+  CorrectFrameHeight;
+end;
+
+
+destructor TParamFrameDirectoryListModEditorFrame.Destroy;
+begin
+  ClearCheckBoxList;
+
+  inherited Destroy;
+end;
+
+
+procedure TParamFrameDirectoryListModEditorFrame.UpdateInterface;
+begin
+  CreateCheckBoxList(FItems);
+  CorrectFrameHeight;
+end;
+
+
+
+end.
+
---
+++ App/ParamFrames/ParamFrameDirectoryList/ParamFrameDirectoryListUnit.pas
@@ -0,0 +1,130 @@
+unit ParamFrameDirectoryListUnit;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Controls, Graphics, Dialogs, StdCtrls, Buttons,
+  StartParamSimple, StartParamDirectoryList,
+  ParamFrameSimpleUnit, ParamFrameDirectoryListModEditorUnit;
+
+type
+  TParamFrameDirectoryListFrame = class(TParamFrameSimpleFrame)
+    btnOpenDirectory: TSpeedButton;
+    btnSelectDirectory: TSpeedButton;
+    btnClearValue: TSpeedButton;
+    cbFullPath: TCheckBox;
+    edValue: TEdit;
+    SelectDirectoryDialog: TSelectDirectoryDialog;
+    procedure btnClearValueClick(Sender: TObject);
+    procedure btnOpenDirectoryClick(Sender: TObject);
+    procedure btnSelectDirectoryClick(Sender: TObject);
+    procedure cbFullPathClick(Sender: TObject);
+  private
+    FModList: TParamFrameDirectoryListModEditorFrame;
+
+    procedure SetValue(AValue: String);
+
+    procedure UpdateHeight;
+  protected
+    procedure PrepareInterface(AItem: TStartParamSimple); override;
+
+  public
+    constructor Create(AItem: TStartParamSimple); reintroduce;
+    destructor  Destroy; override;
+  end;
+
+
+implementation
+
+{$R *.lfm}
+
+uses
+  DayZUtils;
+
+
+procedure TParamFrameDirectoryListFrame.btnSelectDirectoryClick(Sender: TObject);
+begin
+  if SelectDirectoryDialog.Execute then
+  begin
+    SetValue(SelectDirectoryDialog.FileName);
+  end;
+end;
+
+
+procedure TParamFrameDirectoryListFrame.btnClearValueClick(Sender: TObject);
+begin
+  SetValue('');
+end;
+
+
+procedure TParamFrameDirectoryListFrame.btnOpenDirectoryClick(Sender: TObject);
+var
+  Dir: String;
+begin
+  Dir := Trim((FItem as TStartParamDirectoryList).Value);
+  if DirectoryExists(Dir) then
+    OpenFolderInExplorer(Dir);
+end;
+
+
+procedure TParamFrameDirectoryListFrame.cbFullPathClick(Sender: TObject);
+begin
+  (FItem as TStartParamDirectoryList).FullPath := cbFullPath.Checked;
+end;
+
+
+procedure TParamFrameDirectoryListFrame.SetValue(AValue: String);
+begin
+  (FItem as TStartParamDirectoryList).Value := AValue;
+  edValue.Text := AValue;
+  FModList.UpdateInterface;
+  UpdateHeight;
+end;
+
+
+procedure TParamFrameDirectoryListFrame.UpdateHeight;
+begin
+  Self.Height := FModList.Top + FModList.Height + 5;
+end;
+
+
+procedure TParamFrameDirectoryListFrame.PrepareInterface(AItem: TStartParamSimple);
+begin
+  inherited PrepareInterface(AItem);
+
+  edValue.Text := (AItem as TStartParamDirectoryList).Value;
+  cbFullPath.Checked := (AItem as TStartParamDirectoryList).FullPath;
+
+  FModList.UpdateInterface;
+  UpdateHeight;
+end;
+
+
+constructor TParamFrameDirectoryListFrame.Create(AItem: TStartParamSimple);
+begin
+  FModList := TParamFrameDirectoryListModEditorFrame.Create((AItem as TStartParamDirectoryList).DirectoryList);
+
+  inherited Create(AItem);
+
+  FModList.Parent := ContentPanel;
+  FModList.Top := edValue.Top + edValue.Height;
+  FModList.Left := 180;
+  FModList.Width := Self.Width - FModList.Left - 10;
+  FModList.UpdateInterface;
+  UpdateHeight;
+end;
+
+
+destructor TParamFrameDirectoryListFrame.Destroy;
+begin
+  FModList.Free;
+
+  inherited Destroy;
+end;
+
+
+
+end.
+
