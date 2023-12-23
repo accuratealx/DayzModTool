a0ad2637ea0f174a74638495f6f1a4ee53eaf6e3
accuratealx <accuratealx@gmail.com>
2023-12-23 16:57:37 +0300

    Добавлен базовый фрейм закладок

---
+++ App/TabFrames/TabCommonUnit.pas
@@ -0,0 +1,23 @@
+unit TabCommonUnit;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls;
+
+type
+  TTabCommonFrame = class(TFrame)
+  public
+
+  end;
+  TTabCommonFrameClass = class of TTabCommonFrame;
+
+
+implementation
+
+{$R *.lfm}
+
+end.
+
