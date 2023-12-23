9ef317c30db204834ff915c952efd5b2ae6f34d4
accuratealx <accuratealx@gmail.com>
2023-12-23 16:54:58 +0300

    Добавлен базовый фрейм редактора параметра

---
+++ App/ParamFrames/ParamFrameCommonUnit.pas
@@ -0,0 +1,24 @@
+unit ParamFrameCommonUnit;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, ExtCtrls;
+
+type
+  TParamFrameCommonFrame = class(TFrame)
+    ContentPanel: TPanel;
+  public
+  end;
+
+
+implementation
+
+{$R *.lfm}
+
+
+
+end.
+
